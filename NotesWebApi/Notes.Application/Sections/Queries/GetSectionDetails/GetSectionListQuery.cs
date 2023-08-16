e59df35a43eed8b251c1e28a6f795387abe74b04
amir- <amir-bilalov05@mail.ru>
2023-08-16 21:21:06 +0300

    fweiufidh

--- NotesWebApi/Notes.Application/Sections/Queries/GetSectionDetails/GetSectionListQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace Notes.Application.Sections.Queries.GetSectionDetails
-{
-    public class GetSectionListQuery : IRequest<SectionListVm>
-    {
-        public Guid RoomId { get; set; }
-    }
-}
